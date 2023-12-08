849f45bc7fa1a9bee33baba6971070cb037f8621
Dan <507360715@qq.com>
2023-12-08 14:46:14 +0800

    Now, it can run

--- profc.py
+++ profc.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
-import openai
+import time
+from openai import OpenAI
 
 def load_api_key(filepath):
     with open(filepath, 'r') as file:
-        for line in file:
-            if line.startswith('OPENAI_API_KEY='):
-                return line.strip().split('=')[1]
-    return None
+        return file.read().strip()
 
 def main():
     # Load OpenAI API key from config file
@@ -15,7 +13,25 @@ def main():
         print("API key not found in config file.")
         return
 
-    openai.api_key = api_key
+    client = OpenAI(api_key=api_key)
+
+    # Load AI Assistants Exist
+    assistants = client.beta.assistants.list(
+        order="desc",
+        limit="20",
+    )
+
+    # print("assistants.data is ", assistants.data)
+
+    for k, v in enumerate(assistants.data):
+        print(f"Index {k}, name {v.name}, desc {v.description}.")
+    
+
+    index = int(input("Please choose the AI you need: "))
+
+    a_id = assistants.data[index].id
+
+    thread = client.beta.threads.create()
 
     # Welcome message
     print("Welcome to the OpenAI Assistant. Type 'quit' to exit.")
@@ -26,16 +42,34 @@ def main():
         if user_input.lower() == 'quit':
             break
 
-        # Create a request for the Assistant
-        response = openai.Completion.create(
-            model="gpt-4-1106-preview",
-            prompt=user_input,
-            max_tokens=150,
-            stop=None
+        message = client.beta.threads.messages.create(
+            thread_id = thread.id,
+            role="user",
+            content=user_input,
         )
 
-        # Print the response from the Assistant
-        print("Assistant:", response.choices[0].text.strip())
+        run = client.beta.threads.runs.create(
+            thread_id = thread.id,
+            assistant_id = a_id,
+        )
+
+        while True:
+            run = client.beta.threads.runs.retrieve(
+                thread_id=thread.id,
+                run_id=run.id
+            )
+
+            # Check if the run status is 'completed'
+            if run.status == 'completed':
+                thread_messages = client.beta.threads.messages.list(thread.id)
+                # print(thread_messages)
+
+                if thread_messages:
+                    print(f"Ans: {thread_messages.data[0].content[0].text.value}")
+                break
+
+            time.sleep(1)
+
 
 if __name__ == "__main__":
     main()
